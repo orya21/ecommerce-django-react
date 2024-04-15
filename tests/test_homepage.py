64c33565f915d21d62dc1216b2ad5e2a119d0bb2
orya21 <157298840+orya21@users.noreply.github.com>
2024-04-15 16:55:35 +0300

    Update test_homepage.py

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,17 +1,7 @@
-from selenium import webdriver
+from django.test import TestCase
+from django.urls import reverse
 
-def test_homepage_title():
-    # Start the WebDriver (in this example, we'll use Chrome)
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-
-    try:
-        # Open the homepage of your Django application
-        driver.get("http://ecommerce-django-react.herokuapp.com")  # Fixed the URL
-
-        # Check if the page title is correct
-        assert "Ecommerce Django React" in driver.title
-
-    finally:
-        # Close the WebDriver session
-        driver.quit()
+class HomepageTest(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get(reverse('home'))  # Assuming 'home' is the name of your homepage URL
+        self.assertEqual(response.status_code, 200)  # Check if the response status code is 200 (OK)
