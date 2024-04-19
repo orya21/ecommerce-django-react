1686384fcbc253c2c61e03f7ebbdebf93cceb900
orya21 <157298840+orya21@users.noreply.github.com>
2024-04-19 08:49:24 +0300

    Delete tests/test_homepage.py

--- tests/test_homepage.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-class HomepageTest(TestCase):
-    def test_homepage_status_code(self):
-        response = self.client.get(reverse('home'))  # Assuming 'home' is the name of your homepage URL
-        self.assertEqual(response.status_code, 200)  # Check if the response status code is 200 (OK)
