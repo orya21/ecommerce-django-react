fec8dfda16aef2091d17d5d0d1593532227e4778
orya21 <157298840+orya21@users.noreply.github.com>
2024-04-21 22:10:39 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-settings.configure({},DEBUG=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
